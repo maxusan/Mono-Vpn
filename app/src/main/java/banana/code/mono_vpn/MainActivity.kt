52fa604d7a835f271cadd964a4126322d37f2a5c
maxusan <maxusan@gmail.com>
2022-08-06 16:54:38 +0300

    Initial commit

---
+++ app/src/main/java/banana/code/mono_vpn/MainActivity.kt
@@ -0,0 +1,11 @@
+package banana.code.mono_vpn
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
